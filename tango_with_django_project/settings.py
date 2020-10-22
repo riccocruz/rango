95128d9d920312677cf4e820320c67286cc15b17
myusername <rucruz@uci.edu>
2020-10-22 01:41:57 -0700

    change allowed_hosts

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://riccoveigh-rango.herokuapp.com/"]
+ALLOWED_HOSTS = ["riccoveigh-rango.herokuapp.com"]
 
 LOGGING = {
     'version': 1,
